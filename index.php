fca848ec4de7fdeff5132a4e2e2b8d187b70af36
yashjstechno123 <78955804+yashjstechno123@users.noreply.github.com>
2021-05-04 22:28:21 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1>Learn PHP</h1>
+</body>
+
+</html>
+<?php
+$txt = "PHP";
+echo "Hello World ".$txt."!!<br>";
+?>
